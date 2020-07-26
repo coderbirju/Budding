0881cd0c4795ff58c886aef47d76cb853fc1201e
Shrikrishna Joisa <shrikrishnajois@gmail.com>
2020-07-26 18:00:45 +0530

    Fixes issue #1
    - split view
    - added few components

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,14 +3,20 @@ import { Row, Col } from 'antd';
 import 'antd/dist/antd.css';
 import Home from './left/home';
 import Details from './right/details'
+import './main.css'
 
 class mainComponent extends Component {
     state = {  }
     render() {
         return (
-            <div>
-            <Home/>
-            <Details/>
+            <div className="main-body">
+                <Row>
+                    <Col span={12}>
+                    <Home/>
+                    </Col>
+                    <Col span={12}><Details/></Col>
+                </Row>
+
             </div>
           );
     }
