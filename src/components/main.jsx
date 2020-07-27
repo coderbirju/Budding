b4e768c7c9f5e95fea5c10eb9e0c009b0bec1a50
Shrikrishna Joisa <shrikrishnajois@gmail.com>
2020-07-27 23:06:00 +0530

    Layout fix and small updates

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import { Row, Col } from 'antd';
-import 'antd/dist/antd.css';
-import Home from './left/home';
-import Details from './right/details'
-import './main.css'
+import React, { Component } from "react";
+import { Row, Col } from "antd";
+import "antd/dist/antd.css";
+import Home from "./left/home";
+import Details from "./right/details";
+import "./main.css";
 
 class mainComponent extends Component {
-    state = {  }
-    render() {
-        return (
-            <div className="main-body">
-                <Row>
-                    <Col span={12}>
-                    <Home/>
-                    </Col>
-                    <Col span={12}><Details/></Col>
-                </Row>
-
-            </div>
-          );
-    }
+  state = {};
+  render() {
+    return (
+      <Row className="main-row">
+        <Col xs={24} xl={12}>
+          <Home />
+        </Col>
+        <Col xs={24} xl={12}>
+          <Details />
+        </Col>
+      </Row>
+    );
+  }
 }
 
 export default mainComponent;
--- src/components/right/skill.jsx
+++ src/components/right/skill.jsx
@@ -3,15 +3,17 @@ import { Card } from "antd";
 import "./skill.css";
 
 const SkillCard = () => {
-  return <div>
+  return (
+    <div>
       <Card className="s-card-body">
-      <div>
-          <img src="https://picsum.photos/200" alt=""/>
-      </div>
-      <h1 className='s-heading'>Java</h1>
-      <p className='s-desc'> This is the desc for blah blah blah blah </p>
-  </Card>
-  </div>
+        <div>
+          <img src="https://picsum.photos/200" alt="" />
+        </div>
+        <h1 className="s-heading">Java</h1>
+        <p className="s-desc"> This is the desc for blah blah blah blah </p>
+      </Card>
+    </div>
+  );
 };
 
 export default SkillCard;
