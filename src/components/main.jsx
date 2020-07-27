aed7c2d9a56119766e751e3b1a9e98f1639ec830
Arjun Raja <arjunraja@cybrilla.com>
2020-07-27 23:17:23 +0530

    added axios

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,24 +2,60 @@ import React, { Component } from 'react';
 import { Row, Col } from 'antd';
 import 'antd/dist/antd.css';
 import Home from './left/home';
-import Details from './right/details'
-import './main.css'
+import Details from './right/details';
+import './main.css';
+import axios from '../axios';
 
 class mainComponent extends Component {
-    state = {  }
-    render() {
-        return (
-            <div className="main-body">
-                <Row>
-                    <Col span={12}>
-                    <Home/>
-                    </Col>
-                    <Col span={12}><Details/></Col>
-                </Row>
+  constructor(props) {
+    super(props);
+    this.state = {
+      Intro: null,
+      nav: null,
+      social: null,
+    };
+  }
+  getHomeData() {
+    axios
+      .get('home')
+      .then((res) => {
+        const data = res.data;
+        this.setState({ Intro: data.projects[0] });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  getIntroData() {
+    axios
+      .get('contact')
+      .then((res) => {
+        const data = res.data;
+        this.setState({ social: data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  componentDidMount() {
+    this.getHomeData();
+    this.getIntroData();
+  }
 
-            </div>
-          );
-    }
+  render() {
+    return (
+      <div className="main-body">
+        <Row>
+          <Col span={12}>
+            <Home intro={this.state.Intro} social={this.state.social} />
+          </Col>
+          <Col span={12}>
+            <Details />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default mainComponent;
