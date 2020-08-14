67d2b3ff3b35e5c4fe0c435f335d8f576e59b74e
Shrikrishna Joisa <shrikrishnajois@gmail.com>
2020-08-14 22:35:47 +0530

    Fixed nav issue in nav.jsx and social.jsx

--- src/components/left/nav.jsx
+++ src/components/left/nav.jsx
@@ -1,30 +1,19 @@
-import React from 'react';
-import './nav.css';
+import React from "react";
+import "./nav.css";
 // import { menu } from 'antd'
 
 const Navbar = (props) => {
   return (
-    <div className="hidden md:block ">
-      <ul className="ulclass flex flex-row md:flex-col md:-my-16">
-        <li className="liclass md:my-16">
-          <a
-            className="inline-flex items-center hover:text-zenith transition"
-            href="#projects"
-          >
-            <span>01</span>
-            <span className="inline-block w-48 h-px mx-16 transition"></span>
-            <span>Projects</span>
-          </a>
+    <div className="">
+      <ul className="nav-ul">
+        <li className="nav-li">
+          <a className="nav-a nav-transition ">01 project</a>
         </li>
-        <li className="liclass md:my-16">
-          <a
-            className="inline-flex items-center hover:text-zenith transition"
-            href="#projects"
-          >
-            <span>02</span>
-            <span className="inline-block w-48 h-px mx-16 transition"></span>
-            <span>Skills</span>
-          </a>
+        <li className="nav-li">
+          <a className="nav-a nav-transition">02 skills</a>
+        </li>
+        <li className="nav-li">
+          <a className="nav-a nav-transition">03 Resume</a>
         </li>
       </ul>
     </div>
--- src/components/left/social.jsx
+++ src/components/left/social.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 // import 'antd/dist/antd.css';
-import './social.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "./social.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithub,
   faLinkedin,
@@ -10,56 +10,82 @@ import {
   faTwitter,
   faSpotify,
   faSoundcloud,
-} from '@fortawesome/free-brands-svg-icons';
-import profileImg from './static/avatar.png';
+} from "@fortawesome/free-brands-svg-icons";
+import profileImg from "./static/avatar.png";
 
 const Social = (props) => {
   return (
-    <div>
-      <div className="p-stars">
-        <img src={profileImg} alt="ProfilePic" className="avatar" />
-        <span className="p-star-number">
-          <ul>
-            <li>
-              <a href={props.social ? props.social.github : '#'}>
+    <div className="social-div">
+      <div className="content-holder">
+        <img src={profileImg} alt="ProfilePic" className="social-avatar" />
+        <span className="social-span">
+          <ul className="social-ul">
+            <li className="social-li">
+              <a
+                className="social-a"
+                target="_blank"
+                href={props.social ? props.social.github : "#"}
+              >
                 <FontAwesomeIcon icon={faGithub} />
               </a>
             </li>
-            <li>
-              <a href={props.social ? props.social.linkedIn : '#'}>
+            <li className="social-li">
+              <a
+                className="social-a"
+                target="_blank"
+                href={props.social ? props.social.linkedIn : "#"}
+              >
                 <FontAwesomeIcon icon={faLinkedin} />
               </a>
             </li>
-            <li>
+            <li className="social-li">
               <a
+                className="social-a"
+                target="_blank"
                 href={
                   props.social
                     ? props.social.email
                       ? props.social.email
-                      : '#'
-                    : '#'
+                      : "#"
+                    : "#"
                 }
               >
                 <FontAwesomeIcon icon={faGoogle} />
               </a>
             </li>
-            <li>
-              <a href={props.social ? props.social.instagram : '#'}>
+            <li className="social-li">
+              <a
+                className="social-a"
+                target="_blank"
+                href={props.social ? props.social.instagram : "#"}
+              >
                 <FontAwesomeIcon icon={faInstagramSquare} />
               </a>
             </li>
-            <li>
-              <a href={props.social ? props.social.twitter : '#'}>
+            <li className="social-li">
+              <a
+                className="social-a"
+                target="_blank"
+                href={props.social ? props.social.twitter : "#"}
+              >
                 <FontAwesomeIcon icon={faTwitter} />
               </a>
             </li>
-            <li>
-              <a href={props.social ? props.social.spotify : '#'}>
+            <li className="social-li">
+              <a
+                className="social-a"
+                target="_blank"
+                href={props.social ? props.social.spotify : "#"}
+              >
                 <FontAwesomeIcon icon={faSpotify} />
               </a>
             </li>
-            <li>
-              <a href={props.social ? props.social.soundcloud : '#'}>
+            <li className="social-li">
+              <a
+                className="social-a"
+                target="_blank"
+                href={props.social ? props.social.soundcloud : "#"}
+              >
                 <FontAwesomeIcon icon={faSoundcloud} />
               </a>
             </li>
